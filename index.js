e88bf9895a5359a5302161971c753ab3a4bb5feb
braedongeorge <braedonreid@gmail.com>
2015-10-31 15:22:45 +1100

    Added different operations and test cases

--- index.js
+++ index.js
@@ -3,22 +3,42 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-modify-numbers', function (opts) {
     opts = opts || {};
 
-    // Work with options here
-    var myprop = opts.property;
-    var mynumber = opts.number;
-    // var myoperation = opts.operation;
+    var options = {};
+    options.myOperation =
+        opts.operation !== undefined ? opts.operation : null;
+    options.myProp = opts.property !== undefined ? opts.property : null;
+    options.myNumber = opts.number !== undefined ? opts.number : null;
+    options.myFixed = opts.decimals !== undefined ? opts.decimals : 3;
 
-    return function (css, result) {
-        css.walkDecls(myprop, function (decl) {
+    return function (css) {
+        css.walkDecls(options.myProp, function (decl) {
             var val = decl.value;
             var unit = val.match(/[^0-9]+/);
             var pattern = new RegExp('/' + unit + '/');
-            val = val.replace(pattern,'');
-            val = parseInt(val) - parseInt(mynumber);
-            return decl.value = val + unit;
-        });
+            val = val.replace(pattern, '');
 
-        // Transform CSS AST here
+            switch (options.myOperation) {
+            case 'add':
+                val = parseFloat(val) + parseFloat(options.myNumber);
+                break;
+            case 'subtract':
+                val = parseFloat(val) - parseFloat(options.myNumber);
+                break;
+            case 'multiply':
+                val = parseFloat(val) * parseFloat(options.myNumber);
+                break;
+            case 'divide':
+                val = parseFloat(val) / parseFloat(options.myNumber);
+                break;
+            default:
+                console.log('Invalid operation value');
+                break;
+            }
+            val = +val.toFixed(options.myFixed);
+            decl.value = val + unit;
+
+            return decl.value;
+        });
 
     };
 });
--- test/test.js
+++ test/test.js
@@ -1,31 +1,59 @@
 var postcss = require('postcss');
-var expect  = require('chai').expect;
+var assert  = require('assert');
 
 var plugin = require('../');
 
-var input = '.rule { width: 60px } .second { width: 90px }';
-
-var test = function (input, output, opts, done) {
-    postcss([ plugin(opts) ]).process(input).then(function (result) {
-        expect(result.css).to.eql(output);
-        expect(result.warnings()).to.be.empty;
-        done();
-    }).catch(function (error) {
-        done(error);
-    });
-};
+var example = '.rule { width: 60px } .second { height: 90px }';
 
 describe('postcss-modify-all', function () {
 
-    it('should add 2 to the width', function (done) {
-        var output = '.rule { width: 30px } .second { width: 60px }';
-        options = {
-            property: 'width',
-            number: 30,
-            operation: 'subtract'
-        }
+    it('should add 30 to the width', function () {
+        var output = '.rule { width: 90px } .second { height: 90px }';
+        var options = {
+            property:  'width',
+            number:    30,
+            operation: 'add'
+        };
 
-        test(input, output, options, done);
+        var processed = postcss([ plugin(options) ]).process(example).css;
+        assert.equal(output, processed);
+
     });
 
+
+    it('should subtract 5.5 to the height', function () {
+        var output = '.rule { width: 60px } .second { height: 84.5px }';
+        var options = {
+            property:  'height',
+            number:    5.5,
+            operation: 'subtract'
+        };
+
+        var processed = postcss([ plugin(options) ]).process(example).css;
+        assert.equal(output, processed);
+    });
+
+    it('should divide the width by 1.5', function () {
+        var output = '.rule { width: 40px } .second { height: 90px }';
+        var options = {
+            property:  'width',
+            number:    1.5,
+            operation: 'divide'
+        };
+
+        var processed = postcss([ plugin(options) ]).process(example).css;
+        assert.equal(output, processed);
+    });
+
+    it('should multiply the height by 4.53', function () {
+        var output = '.rule { width: 60px } .second { height: 407.7px }';
+        var options = {
+            property:  'height',
+            number:    4.53,
+            operation: 'multiply'
+        };
+
+        var processed = postcss([ plugin(options) ]).process(example).css;
+        assert.equal(output, processed);
+    });
 });
